0bd29b84251d982de09426d5bd346b2b48b19ca9
Lucas Enoki <enoki.lucas@gmail.com>
2023-03-31 16:37:05 -0300

    Caesar_Cipher

---
+++ ruby/01-Caesar_Cipher.rb
@@ -0,0 +1,18 @@
+print "Please, type/paste your text: "
+original = gets.chomp
+
+codes = Array.new()
+
+original.each_char do |letter|
+	codes.push(letter.codepoints[0] + 5)
+end
+
+codes.map do |code|
+	print code.chr
+end
+
+=begin
+	Change each letter of the string to one 5 in advance.
+
+	It also change the spaces.
+=end
