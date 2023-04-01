7daa66c659d81dd8903ec9b9c583474f7ab49f30
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-01 13:16:29 -0300

    Fix downcase

--- ruby/02-Sub_strings.rb
+++ ruby/02-Sub_strings.rb
@@ -1,14 +1,16 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+=begin
 print "type your text:"
 yourText = gets.chomp
+=end
 
 def substrings(text, dictionary)
-	textArray = text.split(' ')
+	textArray = text.downcase.split(' ')
 	textArray.each do |word|
 		dictionary.each do |test|
 			if word == test
-				print word
+				puts word
 			end
 		end
 	end
