c905198a4b92c281f73e095fbc66481144b33905
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-03 17:18:21 -0300

    Complete bubble sort

--- ruby/04-Bubble_Sort.rb
+++ ruby/04-Bubble_Sort.rb
@@ -1,11 +1,20 @@
 Unsorted = [5,8,3,9,6,7,2,1,4]
 
 def bubble_sort(unsort)
-	for i in 0...unsort.length
-		if(unsort[i] > unsort[i+1])
-			x = unsort[i]
-			unsort[i] = unsort[i+1]
-			unsort[i+1] = x
+	sorted = false
+	while sorted == false do
+		sorted = true
+		for i in 0...unsort.length-1
+			if(unsort[i] > unsort[i+1])
+				x = unsort[i]
+				unsort[i] = unsort[i+1]
+				unsort[i+1] = x	
+				sorted = false
+			end
 		end
+
 	end
+	print unsort
 end
+
+bubble_sort(Unsorted)
