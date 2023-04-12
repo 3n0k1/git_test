1e27b85843c8a407cd79f3a9a99a070c3e272695
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-12 15:23:31 -0300

    Create all the methods from the Assignment All still empty

--- linkedList/lib/LinkedList.rb
+++ linkedList/lib/LinkedList.rb
@@ -7,4 +7,19 @@ class LinkedList
 	end
 
 	def append(value) #adds a new node containing value to the end of the list
+
+	end
+
+	def prepend(value) #adds a new node containing value to the start of the list
+
+	end
+
+	def size #returns the total number of nodes in the list
+	def head #returns the first node in the list	
+	def tail #returns the last node in the list
+	def at(index) #returns the node at the given index
+	def pop #removes the last element from the list
+	def contains?(value) #returns true if the passed in value is in the list and otherwise returns false.
+	def find(value) #returns the index of the node containing value, or nil if not found.
+	def to_s #represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
 end
