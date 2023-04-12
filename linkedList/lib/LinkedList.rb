bf2654ed2fa9326d4d3697e280158305a2272497
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-12 16:25:42 -0300

    Fix the methods from LinkedList Create main.rb to test

--- linkedList/lib/LinkedList.rb
+++ linkedList/lib/LinkedList.rb
@@ -30,19 +30,28 @@ class LinkedList
 	def size #returns the total number of nodes in the list
 		number_of_nodes = 1
 		current_node = @head
-		unless current_node.next_node.nil?
+
+		until current_node.next_node.nil?
 			current_node = current_node.next_node
 			number_of_nodes += 1
 		end
+
 		return number_of_nodes
 	end
 
-	
-	def head #returns the first node in the list	
+
+	def head #returns the first node in the list
+	end
 	def tail #returns the last node in the list
+	end
 	def at(index) #returns the node at the given index
+	end
 	def pop #removes the last element from the list
+	end
 	def contains?(value) #returns true if the passed in value is in the list and otherwise returns false.
+	end
 	def find(value) #returns the index of the node containing value, or nil if not found.
+	end
 	def to_s #represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+	end
 end
