16b74cd935dd6f6a9b660d6f1d026ade3b8b8411
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-12 20:37:13 -0300

    3) prepared the array

--- BST/lib/tree.rb
+++ BST/lib/tree.rb
@@ -1,4 +1,4 @@
-
+arr1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 class Tree
 	attr_accessor :root
@@ -8,6 +8,14 @@ class Tree
 	end
 
 	def built_tree(arr)
+		arr = arr.sort.uniq
+		p arr.inspect
+
+		if arr.lenght % 2 = 0
+			p 'even'
+		else
+			p 'odd'
+		end
 		
 	end
 end
