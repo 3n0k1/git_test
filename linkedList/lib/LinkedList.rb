fcbfce4d7cd7aff22339a251775ab2af14817eee
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-12 16:10:00 -0300

    methods: 'append' and 'prepend' 'size' still not tested

--- linkedList/lib/LinkedList.rb
+++ linkedList/lib/LinkedList.rb
@@ -1,20 +1,43 @@
+require_relative 'Node'
+
 class LinkedList
 	def initialize
-		@list = new.Array
-		@head
+		@head = Node.new
 		@tail
-		@node
 	end
 
 	def append(value) #adds a new node containing value to the end of the list
+		new_node = Node.new(value)
 
+    if @head.next_node.nil?
+      @head = new_node
+    else
+      current_node = @head
+      unless current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
 	end
 
 	def prepend(value) #adds a new node containing value to the start of the list
+		new_node = Node.new(value)
+		new_node.next_node = @head
+    @head = new_node
 
 	end
 
 	def size #returns the total number of nodes in the list
+		number_of_nodes = 1
+		current_node = @head
+		unless current_node.next_node.nil?
+			current_node = current_node.next_node
+			number_of_nodes += 1
+		end
+		return number_of_nodes
+	end
+
+	
 	def head #returns the first node in the list	
 	def tail #returns the last node in the list
 	def at(index) #returns the node at the given index
