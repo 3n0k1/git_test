95414cea8f4082eb4a7a08b12973ab5fc8dc6aae
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-14 14:50:51 -0300

    pausing

--- BST/lib/tree.rb
+++ BST/lib/tree.rb
@@ -4,18 +4,27 @@ class Tree
 	attr_accessor :root
 
 	def initialize(arr)
+		@arr
 		@root = built_tree(arr)
 	end
 
 	def built_tree(arr)
-		arr = arr.sort.uniq
-		p arr.inspect
+		@arr = arr.sort.uniq
+		p @arr.inspect
+		
+		root_inx = @arr.length/2
+		root_node = @arr[root_inx]
+		
+		return root_node
+	end
 
-		if arr.length % 2 == 0
-			p 'even'
-		else
-			p 'odd'
-		end
+	def insert
+		p @arr.inspect
+	end
+
+	def delete(value)
 
 	end
+
+
 end
--- BST/main.rb
+++ BST/main.rb
@@ -4,3 +4,4 @@ require_relative 'lib/tree'
 arr1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 tree = Tree.new(arr1)
+tree.insert
